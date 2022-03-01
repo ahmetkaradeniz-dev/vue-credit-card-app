78c4f32218ab6f80178930efca350583a36ab803
Ahmet Karadeniz <76037296+ahmetkaradeniz-dev@users.noreply.github.com>
2022-03-02 00:29:14 +0300

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/Home.vue')
     }
   ]
 })
